44a4c9939704bc8e2fcc4303f297f9a3789cf761
Daniel Vartanian <danielvartan@proton.me>
2025-08-13 01:26:30 -0300

    Add `og-image.png` to `docs`

--- R/_post-render.R
+++ R/_post-render.R
@@ -1,6 +1,14 @@
 # Load Packages -----
 
 library(beepr)
+library(fs)
+library(here)
+
+# Add `og-image.png` to `./docs/images` -----
+
+file <- here("docs", "images", "og-image.png")
+
+file_copy(here("images", "og-image.png"), file, overwrite = TRUE)
 
 # Check If the Script Ran Successfully -----
 
